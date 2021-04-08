cc024c9d1bb10c5192877b7d5d4bd784d439a7e7
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-04-08 11:58:54 -0300

    Scale of colors pending

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import $ from 'jquery';
+import Colors from './components/Colors';
 
 class App extends Component {
   constructor(props) {
@@ -6,11 +8,36 @@ class App extends Component {
     this.state = {
       color: '#f15025'
     }
+
+    this.submit = this.submit.bind(this);
   }
+
+  componentDidMount() {
+    $('#submit-btn').css("background-color", this.state.color);
+  }
+
+  componentDidUpdate() {
+    $('#submit-btn').css("background-color", this.state.color);
+  }
+
+  submit() {
+    this.setState({
+      color: document.getElementById('input-text').value
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="header">
+          <div id="title"><h1>Color generator</h1></div>
+          <div id="controls">
+            <input id="input-text" placeholder={this.state.color} type="text"></input>
+            <button onClick={this.submit} id="submit-btn">Submit</button>
+          </div>
+        </div>
 
+        <Colors data={this.state.color} />
       </div>
     );
   }
---
+++ src/components/Colors.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Colors extends Component {
+    render() {
+        return (
+            <div className="colors-container">
+
+            </div>
+        )
+    }
+}
